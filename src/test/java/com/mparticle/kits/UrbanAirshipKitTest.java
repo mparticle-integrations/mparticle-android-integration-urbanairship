1a10728dd8429e27aac0c02b7ee9291ca11b613a
Sam Dozor <sdozor@mparticle.com>
2016-09-13 13:14:44 -0400

    Add base unit tests

--- src/test/java/com/mparticle/kits/UrbanAirshipKitTest.java
+++ src/test/java/com/mparticle/kits/UrbanAirshipKitTest.java
@@ -1,5 +1,7 @@
 package com.mparticle.kits;
 
+import android.content.Context;
+
 import com.mparticle.MPEvent;
 import com.mparticle.MParticle;
 import com.mparticle.commerce.CommerceEvent;
@@ -17,9 +19,52 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class UrbanAirshipKitTest {
+
+    private KitIntegration getKit() {
+        return new UrbanAirshipKit();
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        String name = getKit().getName();
+        assertTrue(name != null && name.length() > 0);
+    }
+
+    /**
+     * Kit *should* throw an exception when they're initialized with the wrong settings.
+     *
+     */
+    @Test
+    public void testOnKitCreate() throws Exception{
+        Exception e = null;
+        try {
+            KitIntegration kit = getKit();
+            Map settings = new HashMap<>();
+            settings.put("fake setting", "fake");
+            kit.onKitCreate(settings, Mockito.mock(Context.class));
+        }catch (Exception ex) {
+            e = ex;
+        }
+        assertNotNull(e);
+    }
+
+    @Test
+    public void testClassName() throws Exception {
+        KitIntegrationFactory factory = new KitIntegrationFactory();
+        Map<Integer, String> integrations = factory.getKnownIntegrations();
+        String className = getKit().getClass().getName();
+        for (Map.Entry<Integer, String> entry : integrations.entrySet()) {
+            if (entry.getValue().equals(className)) {
+                return;
+            }
+        }
+        fail(className + " not found as a known integration.");
+    }
 
     @Test
     public void testParsing() throws Exception {
