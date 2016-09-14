c288a9600b1cff3fd2f143a1e35a53d763824900
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2016-09-14 16:07:44 -0700

    Update to Urban Airship SDK 8.0.0

--- src/main/java/com/mparticle/kits/UrbanAirshipKit.java
+++ src/main/java/com/mparticle/kits/UrbanAirshipKit.java
@@ -3,7 +3,6 @@ package com.mparticle.kits;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.WakefulBroadcastReceiver;
 
 import com.mparticle.MPEvent;
 import com.mparticle.MParticle;
@@ -13,8 +12,10 @@ import com.urbanairship.Autopilot;
 import com.urbanairship.UAirship;
 import com.urbanairship.analytics.CustomEvent;
 import com.urbanairship.analytics.InstallReceiver;
+import com.urbanairship.analytics.RetailEventTemplate;
+import com.urbanairship.push.GcmConstants;
+import com.urbanairship.push.GcmPushReceiver;
 import com.urbanairship.push.PushMessage;
-import com.urbanairship.push.PushService;
 import com.urbanairship.push.TagEditor;
 
 import java.math.BigDecimal;
@@ -109,22 +110,15 @@ public class UrbanAirshipKit extends KitIntegration implements  KitIntegration.P
 
     @Override
     public void onPushMessageReceived(Context context, Intent intent) {
-        Intent pushIntent = (new Intent(context, PushService.class))
-                .setAction("com.urbanairship.push.ACTION_RECEIVE_GCM_MESSAGE")
-                .putExtra("com.urbanairship.push.EXTRA_INTENT", intent);
-
-        WakefulBroadcastReceiver.startWakefulService(context, pushIntent);
+        new GcmPushReceiver().onReceive(context, intent);
     }
 
     @Override
     public boolean onPushRegistration(String instanceId, String senderId) {
         // Assume mismatch sender Ids means we need to refresh the token
-        if (!senderId.equals(UAirship.shared().getPushManager().getGcmToken())) {
-            Intent intent = new Intent(UAirship.getApplicationContext(), PushService.class)
-                    .setAction("com.urbanairship.push.ACTION_UPDATE_PUSH_REGISTRATION")
-                    .putExtra("com.urbanairship.push.EXTRA_GCM_TOKEN_REFRESH", true);
-
-            UAirship.getApplicationContext().startService(intent);
+        if (!senderId.equals(UAirship.shared().getPushManager().getRegistrationToken())) {
+            Intent intent = new Intent(GcmConstants.ACTION_GCM_REGISTRATION);
+            new GcmPushReceiver().onReceive(getContext(), intent);
         }
 
         return true;
@@ -303,28 +297,36 @@ public class UrbanAirshipKit extends KitIntegration implements  KitIntegration.P
         switch (event.getProductAction()) {
             case Product.PURCHASE:
                 for (Product product : event.getProducts()) {
-                    populateRetailEvent(RetailEvent.createPurchasedEvent(), product).track();
+                    populateRetailEventTemplate(RetailEventTemplate.newPurchasedTemplate(), product)
+                            .createEvent()
+                            .track();
                 }
 
                 break;
 
             case Product.ADD_TO_CART:
                 for (Product product : event.getProducts()) {
-                    populateRetailEvent(RetailEvent.createAddedToCartEvent(), product).track();
+                    populateRetailEventTemplate(RetailEventTemplate.newAddedToCartTemplate(), product)
+                            .createEvent()
+                            .track();
                 }
 
                 break;
 
             case Product.CLICK:
                 for (Product product : event.getProducts()) {
-                    populateRetailEvent(RetailEvent.createBrowsedEvent(), product).track();
+                    populateRetailEventTemplate(RetailEventTemplate.newBrowsedTemplate(), product)
+                            .createEvent()
+                            .track();
                 }
 
                 break;
 
             case Product.ADD_TO_WISHLIST:
                 for (Product product : event.getProducts()) {
-                    populateRetailEvent(RetailEvent.createStarredProduct(), product).track();
+                    populateRetailEventTemplate(RetailEventTemplate.newStarredProductTemplate(), product)
+                            .createEvent()
+                            .track();
                 }
                 break;
             default:
@@ -336,14 +338,14 @@ public class UrbanAirshipKit extends KitIntegration implements  KitIntegration.P
     }
 
     /**
-     * Populates an Urban Airship RetailEvent from a product.
+     * Populates an Urban Airship RetailEventTemplate from a product.
      *
-     * @param event The retail event.
+     * @param template The retail event template.
      * @param product The product.
-     * @return The populated retail event.
+     * @return The populated retail event template.
      */
-    private RetailEvent populateRetailEvent(RetailEvent event, Product product) {
-        return event.setCategory(product.getCategory())
+    private RetailEventTemplate populateRetailEventTemplate(RetailEventTemplate template, Product product) {
+        return template.setCategory(product.getCategory())
                 .setId(product.getSku())
                 .setDescription(product.getName())
                 .setValue(product.getTotalAmount())
